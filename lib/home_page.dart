4892bcef0f6a2806762dd06396cdecb35f3747b8
Ansh Jaiswal <jaiansh2016@gmail.com>
2025-06-09 15:31:31 +0530

    speech to text added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 import 'package:talkie_app/feature_box.dart';
 import 'package:talkie_app/palette.dart';
 
@@ -10,6 +12,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final speechToText = SpeechToText();
+  String lastWords = '';
+  @override
+  void initState() {
+    super.initState();
+    initSpeechToText();
+  }
+
+  Future<void> initSpeechToText() async {
+    await speechToText.initialize();
+    setState(() {});
+  }
+
+  Future<void> startListening() async {
+    await speechToText.listen(onResult: onSpeechResult);
+    setState(() {});
+  }
+
+  Future<void> stopListening() async {
+    await speechToText.stop();
+    setState(() {});
+  }
+
+  void dispose() {
+    super.dispose();
+    speechToText.stop();
+  }
+
+  void onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,95 +54,105 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         leading: Icon(Icons.menu),
       ),
-      body: Column(
-        children: [
-          Stack(
-            //virtaul assistant picture
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.only(bottom: 80), // So FAB doesn't overlap
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Center(
-                child: Container(
-                  height: 120,
-                  width: 120,
-                  margin: const EdgeInsets.only(top: 4),
-                  decoration: BoxDecoration(
-                    color: Pallete.assistantCircleColor,
-                    shape: BoxShape.circle
+              Stack(
+                children: [
+                  Center(
+                    child: Container(
+                      height: 120,
+                      width: 120,
+                      margin: const EdgeInsets.only(top: 4),
+                      decoration: BoxDecoration(
+                        color: Pallete.assistantCircleColor,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 123,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/virtualAssistant.png'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                margin: EdgeInsets.symmetric(horizontal: 40).copyWith(top: 30),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Pallete.borderColor),
+                  borderRadius: BorderRadius.circular(
+                    20,
+                  ).copyWith(topLeft: Radius.zero),
+                ),
+                child: Text(
+                  'Hello, How can I help you?',
+                  style: TextStyle(
+                    color: Pallete.mainFontColor,
+                    fontFamily: 'Cera Pro',
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
               ),
               Container(
-                height: 123,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/virtualAssistant.png')
-                  )
+                padding: EdgeInsets.only(left: 10),
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.only(top: 10, left: 22),
+                child: const Text(
+                  'Here are a few features',
+                  style: TextStyle(
+                    color: Pallete.mainFontColor,
+                    fontFamily: 'Cera Pro',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              )
+              ),
+              FeatureBox(
+                color: Pallete.firstSuggestionBoxColor,
+                headerText: 'ChatGPT',
+                descriptionText:
+                    'A smarter way to stay organized and informed with ChatGPT',
+              ),
+              FeatureBox(
+                color: Pallete.secondSuggestionBoxColor,
+                headerText: 'Dall-E',
+                descriptionText:
+                    'Get inspired and stay creative with your personal assistant powered by Dall-E',
+              ),
+              FeatureBox(
+                color: Pallete.thirdSuggestionBoxColor,
+                headerText: 'Smart Voice Assistant',
+                descriptionText:
+                    'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT',
+              ),
             ],
           ),
-          //chat bubble
-           Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
-            ),
-            margin: EdgeInsets.symmetric(horizontal: 40).copyWith(top: 30),
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Pallete.borderColor,),
-                borderRadius: BorderRadius.circular(20).copyWith(
-                  topLeft: Radius.zero,),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: Text(
-                'Hello, How can I help you?', 
-                style: TextStyle(
-                color: Pallete.mainFontColor,
-                fontFamily: 'Cera Pro',
-                fontSize: 25,
-                fontWeight: FontWeight.w600,
-              ),),
-            ),
-           ),
-           Container(
-            padding: EdgeInsets.only(left:10),
-            alignment: Alignment.centerLeft,
-            margin: EdgeInsets.only(top: 10,left: 22),
-             child: const Text('Here are a few features',
-              style: TextStyle(
-                  color: Pallete.mainFontColor,
-                  fontFamily: 'Cera Pro',
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-             )),
-           ),
-           //features list
-           Column(
-            children: [
-              FeatureBox(
-                color: Pallete.firstSuggestionBoxColor, 
-                headerText: 'Chatgpt', 
-                descriptionText: 'A smarter way to stay organized and informed with ChatGPT',
-              ),
-              FeatureBox(
-                color: Pallete.secondSuggestionBoxColor, 
-                headerText: 'Dall-E', 
-                descriptionText: 'Get inspired and stay creative with your personal assistant powered by Dall-E',
-              ),
-              FeatureBox(
-                color: Pallete.thirdSuggestionBoxColor, 
-                headerText: 'Smart Voice Assistant', 
-                descriptionText: 'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT',
-              ),
-            ],
-           )
-        ]
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Pallete.firstSuggestionBoxColor,
-        onPressed: () {},
+        onPressed: () async {
+          if (await speechToText.hasPermission && speechToText.isNotListening) {
+            await startListening();
+          }else if(speechToText.isListening){
+            await stopListening();
+          }else {
+            await initSpeechToText();
+          }
+        },
         child: Icon(Icons.mic),
       ),
     );
